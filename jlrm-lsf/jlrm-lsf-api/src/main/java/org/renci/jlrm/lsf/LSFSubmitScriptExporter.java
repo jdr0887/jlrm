578209533c753a5ce90d83bb162f85d67091bec5
Jason Reilly <jdr0887@renci.org>
2012-05-21 12:26:32 -0400

    fixing memory...should come from the job, removing export function that is specific to ssh jobs

--- jlrm-lsf/jlrm-lsf-api/src/main/java/org/renci/jlrm/lsf/LSFSubmitScriptExporter.java
+++ jlrm-lsf/jlrm-lsf-api/src/main/java/org/renci/jlrm/lsf/LSFSubmitScriptExporter.java
@@ -1,5 +1,6 @@
 package org.renci.jlrm.lsf;
 
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,53 +15,6 @@ public class LSFSubmitScriptExporter<T extends LSFJob> {
 
     public LSFSubmitScriptExporter() {
         super();
-    }
-
-    public T export(File workDir, String remoteWorkDir, T job) throws IOException {
-        logger.debug("ENTERING export(File, LSFJob)");
-        File submitFile = new File(workDir, String.format("%s.sub", job.getName()));
-
-        FileWriter submitFileWriter = new FileWriter(submitFile);
-
-        submitFileWriter.write("#!/bin/bash\n\n");
-        submitFileWriter.write("set -e\n\n");
-
-        if (StringUtils.isNotEmpty(job.getQueueName())) {
-            submitFileWriter.write(String.format("#BSUB -q %s%n", job.getQueueName()));
-        }
-
-        if (StringUtils.isNotEmpty(job.getProject())) {
-            submitFileWriter.write(String.format("#BSUB -P %s%n", job.getProject()));
-        }
-
-        if (job.getWallTime() != null) {
-            submitFileWriter.write(String.format("#BSUB -W %s%n", job.getWallTime()));
-        }
-
-        if (job.getMemory() != null) {
-            submitFileWriter.write(String.format("#BSUB -M %s%n", job.getMemory() * 1024));
-        }
-        
-        submitFileWriter.write(String.format("#BSUB -i %s%n", "/dev/null"));
-        
-        job.setOutput(new File(String.format("%s/%s.out", remoteWorkDir, job.getOutput().getName())));
-        job.setError(new File(String.format("%s/%s.err", remoteWorkDir, job.getError().getName())));
-
-        submitFileWriter.write(String.format("#BSUB -o %s%n", job.getOutput().getAbsolutePath()));
-        submitFileWriter.write(String.format("#BSUB -e %s%n", job.getError().getAbsolutePath()));
-        submitFileWriter.write(String.format("#BSUB -n %s%n", job.getNumberOfProcessors()));
-
-        if (job.getHostCount() != null) {
-            submitFileWriter.write(String.format("#BSUB -R \"span[hosts=%d]\"%n", job.getHostCount()));
-        }
-
-        submitFileWriter.write(remoteWorkDir + File.separator + job.getExecutable().getName());
-
-        submitFileWriter.flush();
-        submitFileWriter.close();
-        job.setSubmitFile(submitFile);
-
-        return job;
     }
 
     public T export(File workDir, T job) throws IOException {
@@ -83,7 +37,7 @@ public class LSFSubmitScriptExporter<T extends LSFJob> {
             submitFileWriter.write(String.format("#BSUB -W %s%n", job.getWallTime()));
         }
 
-        submitFileWriter.write(String.format("#BSUB -M %s%n", job.getMemory() + 1024));
+        submitFileWriter.write(String.format("#BSUB -M %s%n", job.getMemory()));
         submitFileWriter.write(String.format("#BSUB -i %s%n", "/dev/null"));
         
         job.setOutput(new File(String.format("%s/%s.out", workDir.getAbsolutePath(), job.getOutput().getName())));
