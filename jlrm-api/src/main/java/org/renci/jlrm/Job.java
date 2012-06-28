823de1f2f290be321f474b7cb7407db27e052be3
Jason Reilly <jdr0887@renci.org>
2012-06-28 13:06:41 -0400

    adding hashcode and equals

--- jlrm-api/src/main/java/org/renci/jlrm/Job.java
+++ jlrm-api/src/main/java/org/renci/jlrm/Job.java
@@ -101,4 +101,71 @@ public class Job {
                 + ", memory=" + memory + "]";
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((error == null) ? 0 : error.hashCode());
+        result = prime * result + ((executable == null) ? 0 : executable.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((memory == null) ? 0 : memory.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((numberOfProcessors == null) ? 0 : numberOfProcessors.hashCode());
+        result = prime * result + ((output == null) ? 0 : output.hashCode());
+        result = prime * result + ((submitFile == null) ? 0 : submitFile.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Job other = (Job) obj;
+        if (error == null) {
+            if (other.error != null)
+                return false;
+        } else if (!error.equals(other.error))
+            return false;
+        if (executable == null) {
+            if (other.executable != null)
+                return false;
+        } else if (!executable.equals(other.executable))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (memory == null) {
+            if (other.memory != null)
+                return false;
+        } else if (!memory.equals(other.memory))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (numberOfProcessors == null) {
+            if (other.numberOfProcessors != null)
+                return false;
+        } else if (!numberOfProcessors.equals(other.numberOfProcessors))
+            return false;
+        if (output == null) {
+            if (other.output != null)
+                return false;
+        } else if (!output.equals(other.output))
+            return false;
+        if (submitFile == null) {
+            if (other.submitFile != null)
+                return false;
+        } else if (!submitFile.equals(other.submitFile))
+            return false;
+        return true;
+    }
+
 }
