396b85e8ef59ead8ff3089e4279fb38ec9ee5d99
Jason Reilly <jdr0887@renci.org>
2012-07-09 10:43:47 -0400

    adding check for jobs that are not reported and marking them done

--- jlrm-sge/jlrm-sge-ssh/src/main/java/org/renci/jlrm/sge/ssh/SGESSHLookupStatusCallable.java
+++ jlrm-sge/jlrm-sge-ssh/src/main/java/org/renci/jlrm/sge/ssh/SGESSHLookupStatusCallable.java
@@ -129,7 +129,21 @@ public class SGESSHLookupStatusCallable implements Callable<Set<SGEJobStatusInfo
                     jobStatusSet.add(info);
                 }
             }
+            
+            Set<String> jobIdSet = new HashSet<String>();
+            for (SGEJobStatusInfo info : jobStatusSet) {
+                jobIdSet.add(info.getJobId());
+            }
 
+            if (jobs != null) {
+                for (SGESSHJob job : jobs) {
+                    if (!jobIdSet.contains(job.getId())) {
+                        //need to default the queueName for non existing jobs
+                        jobStatusSet.add(new SGEJobStatusInfo(job.getId(), SGEJobStatusType.DONE, "all.q"));
+                    }
+                }
+            }
+            
         } catch (JSchException e) {
             logger.error("JSchException", e);
             throw new JLRMException("JSchException: " + e.getMessage());
