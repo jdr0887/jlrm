e4e263edbb30526af657a13a335deae393bb3ef2
Jason Reilly <jdr0887@renci.org>
2012-10-24 23:00:09 -0400

    using UUID instead of run dir with count

--- jlrm-api/src/main/java/org/renci/jlrm/IOUtils.java
+++ jlrm-api/src/main/java/org/renci/jlrm/IOUtils.java
@@ -1,35 +1,19 @@
 package org.renci.jlrm;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.text.Format;
 import java.util.Date;
+import java.util.UUID;
 
 public class IOUtils {
 
-    public static File createWorkDirectory(File submitDir, String jobsDirName, String name) {
-        File jobsDir = new File(submitDir, jobsDirName);
-        Date date = new Date();
+    public static File createWorkDirectory(File submitDir, String name) {
         Format formatter = PerThreadDateFormatter.getDateFormatter();
-        File datedDir = new File(jobsDir, formatter.format(date));
-        File jobSubmitDir = new File(datedDir, name);
-        File[] runDirectories = jobSubmitDir.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                if (name.startsWith("run")) {
-                    return true;
-                }
-                return false;
-            }
-        });
-        File runDir = new File(jobSubmitDir, String.format("run%04d", runDirectories != null
-                && runDirectories.length > 0 ? runDirectories.length + 1 : 1));
+        File datedDir = new File(submitDir, formatter.format(new Date()));
+        File namedDir = new File(datedDir, name);
+        File runDir = new File(namedDir, UUID.randomUUID().toString());
         runDir.mkdirs();
         return runDir;
-    }
-
-    public static File createWorkDirectory(File submitDir, String name) {
-        return createWorkDirectory(submitDir, "jobs", name);
     }
 
 }
