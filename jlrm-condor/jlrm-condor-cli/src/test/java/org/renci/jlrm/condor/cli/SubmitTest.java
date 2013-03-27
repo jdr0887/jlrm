2151b9e7492941c62eb47281a966d1be310a1acf
Jason Reilly <jdr0887@renci.org>
2013-03-27 12:37:15 -0400

    using constructor

--- jlrm-condor/jlrm-condor-cli/src/test/java/org/renci/jlrm/condor/cli/SubmitTest.java
+++ jlrm-condor/jlrm-condor-cli/src/test/java/org/renci/jlrm/condor/cli/SubmitTest.java
@@ -58,11 +58,7 @@ public class SubmitTest {
             g.addEdge(job3, job4);
 
             File submitDir = new File("/tmp");
-            CondorSubmitDAGCallable callable = new CondorSubmitDAGCallable();
-            callable.setCondorHomeDirectory(new File(System.getenv("CONDOR_HOME")));
-            callable.setDagName("asdfasdf");
-            callable.setSubmitDir(submitDir);
-            callable.setGraph(g);
+            CondorSubmitDAGCallable callable = new CondorSubmitDAGCallable(submitDir, g, "asdfasdf", false);
             CondorJob result = callable.call();
         } catch (JLRMException e) {
             e.printStackTrace();
