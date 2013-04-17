c75c01e964f82ca662464f89e5b2d369d67e6f56
Jason Reilly <jdr0887@renci.org>
2013-04-17 09:28:04 -0400

    just requiring dagSubmitScript

--- jlrm-condor/jlrm-condor-cli/src/main/java/org/renci/jlrm/condor/cli/CondorSubmitDAGCallable.java
+++ jlrm-condor/jlrm-condor-cli/src/main/java/org/renci/jlrm/condor/cli/CondorSubmitDAGCallable.java
@@ -8,52 +8,33 @@ import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.jgrapht.Graph;
 import org.renci.common.exec.BashExecutor;
 import org.renci.common.exec.CommandInput;
 import org.renci.common.exec.CommandOutput;
 import org.renci.common.exec.Executor;
 import org.renci.common.exec.ExecutorException;
-import org.renci.jlrm.IOUtils;
 import org.renci.jlrm.JLRMException;
-import org.renci.jlrm.condor.CondorJob;
-import org.renci.jlrm.condor.CondorJobEdge;
-import org.renci.jlrm.condor.CondorSubmitScriptExporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CondorSubmitDAGCallable implements Callable<CondorJob> {
+public class CondorSubmitDAGCallable implements Callable<Integer> {
 
     private final Logger logger = LoggerFactory.getLogger(CondorSubmitDAGCallable.class);
 
-    private File submitDir;
+    private File dagSubmitScript;
 
-    private Graph<CondorJob, CondorJobEdge> graph;
-
-    private String dagName;
-
-    private Boolean includeGlideinRequirements = Boolean.FALSE;
-
-    public CondorSubmitDAGCallable(File submitDir, Graph<CondorJob, CondorJobEdge> graph, String dagName,
-            Boolean includeGlideinRequirements) {
+    public CondorSubmitDAGCallable(File dagSubmitScript) {
         super();
-        this.submitDir = submitDir;
-        this.graph = graph;
-        this.dagName = dagName;
-        this.includeGlideinRequirements = includeGlideinRequirements;
+        this.dagSubmitScript = dagSubmitScript;
     }
 
     @Override
-    public CondorJob call() throws JLRMException {
-
-        File workDir = IOUtils.createWorkDirectory(submitDir, this.dagName);
-        CondorSubmitScriptExporter exporter = CondorSubmitScriptExporter.getInstance();
-        CondorJob dagSubmitJob = exporter.export(dagName, workDir, graph, includeGlideinRequirements);
+    public Integer call() throws JLRMException {
 
         try {
 
-            String command = String.format("condor_submit_dag %s", dagSubmitJob.getSubmitFile().getName());
-            CommandInput input = new CommandInput(command, dagSubmitJob.getSubmitFile().getParentFile());
+            String command = String.format("condor_submit_dag %s", dagSubmitScript.getName());
+            CommandInput input = new CommandInput(command, dagSubmitScript.getParentFile());
             input.setExitImmediately(Boolean.FALSE);
             Executor executor = BashExecutor.getInstance();
             CommandOutput output = executor.execute(input, new File(System.getProperty("user.home"), ".bashrc"));
@@ -75,6 +56,7 @@ public class CondorSubmitDAGCallable implements Callable<CondorJob> {
                 throw new JLRMException(sb.toString());
             }
 
+            Integer ret = null;
             while ((line = lnr.readLine()) != null) {
                 if (line.indexOf("submitted to cluster") != -1) {
                     logger.info("line = " + line);
@@ -83,14 +65,12 @@ public class CondorSubmitDAGCallable implements Callable<CondorJob> {
                     if (!matcher.matches()) {
                         throw new JLRMException("failed to parse the cluster number");
                     }
-                    dagSubmitJob.setCluster(Integer.parseInt(matcher.group(2)));
-                    // jobBean.setJobId(Integer.parseInt(matcher.group(1)));
-                    dagSubmitJob.setJobId(0);
+                    ret = Integer.parseInt(matcher.group(2));
                     break;
                 }
             }
 
-            return dagSubmitJob;
+            return ret;
         } catch (NumberFormatException e) {
             e.printStackTrace();
             throw new JLRMException("Failed to parse cluster id: " + e.getMessage());
@@ -102,38 +82,6 @@ public class CondorSubmitDAGCallable implements Callable<CondorJob> {
             throw new JLRMException("IOException: " + e.getMessage());
         }
 
-    }
-
-    public File getSubmitDir() {
-        return submitDir;
-    }
-
-    public void setSubmitDir(File submitDir) {
-        this.submitDir = submitDir;
-    }
-
-    public Graph<CondorJob, CondorJobEdge> getGraph() {
-        return graph;
-    }
-
-    public void setGraph(Graph<CondorJob, CondorJobEdge> graph) {
-        this.graph = graph;
-    }
-
-    public String getDagName() {
-        return dagName;
-    }
-
-    public void setDagName(String dagName) {
-        this.dagName = dagName;
-    }
-
-    public Boolean getIncludeGlideinRequirements() {
-        return includeGlideinRequirements;
-    }
-
-    public void setIncludeGlideinRequirements(Boolean includeGlideinRequirements) {
-        this.includeGlideinRequirements = includeGlideinRequirements;
     }
 
 }
