ae6e2ab6c6aa2fae2d439ae7e355af95ea5d3b1f
Jason Reilly <jdr0887@renci.org>
2013-06-18 13:03:38 -0400

    updating test

--- jlrm-lsf/jlrm-lsf-ssh/src/test/java/org/renci/jlrm/lsf/ssh/LSFSSHFactoryTest.java
+++ jlrm-lsf/jlrm-lsf-ssh/src/test/java/org/renci/jlrm/lsf/ssh/LSFSSHFactoryTest.java
@@ -78,7 +78,7 @@ public class LSFSSHFactoryTest {
             // LSFSSHJob job = lsfSSHFactory.submitGlidein(submitDir, 2, 30, 40, "biodev1.its.unc.edu", "huge");
             // LSFSSHJob job = lsfSSHFactory.submitGlidein(submitDir, 2, 30, 40, "biodev1.its.unc.edu", "week");
             LSFSSHSubmitCondorGlideinCallable callable = new LSFSSHSubmitCondorGlideinCallable();
-            callable.setCollectorHost("gnet641.its.unc.edu");
+            callable.setCollectorHost("biodev2.its.unc.edu");
             callable.setUsername("rc_renci.svc");
             callable.setSite(site);
             callable.setJobName("glidein");
