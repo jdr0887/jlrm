9d61075abf32576ec8d3585eab23487b54f25773
Jason Reilly <jdr0887@renci.org>
2013-09-17 11:25:43 -0400

    cleaning, adding error checks

--- jlrm-condor/jlrm-condor-cli/src/main/java/org/renci/jlrm/condor/cli/CondorLookupDAGStatusCallable.java
+++ jlrm-condor/jlrm-condor-cli/src/main/java/org/renci/jlrm/condor/cli/CondorLookupDAGStatusCallable.java
@@ -10,6 +10,7 @@ import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.renci.jlrm.JLRMException;
 import org.renci.jlrm.condor.CondorJob;
@@ -53,7 +54,6 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
             return ret;
         }
 
-
         BufferedReader br = null;
         try {
 
@@ -78,41 +78,55 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
                     br.readLine();
                     br.readLine();
                     String tallies = br.readLine();
-                    System.out.println(tallies);
                     Pattern pattern = Pattern
                             .compile("^(\\d*/\\d*/\\d*\\s.\\d*:\\d*:\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)");
                     Matcher matcher = pattern.matcher(tallies);
                     boolean matches = matcher.matches();
-                    System.out.println(matches);
                     if (matches) {
 
                         String match = matcher.group(1);
-                        try {
-                            date = DateUtils.parseDate(match, new String[] { "MM/dd/yy HH:mm:ss" });
-                        } catch (ParseException e) {
-                            e.printStackTrace();
+                        if (StringUtils.isNotEmpty(match)) {
+                            try {
+                                date = DateUtils.parseDate(match, new String[] { "MM/dd/yy HH:mm:ss" });
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
                         }
 
                         match = matcher.group(2);
-                        done = Integer.valueOf(match);
+                        if (StringUtils.isNotEmpty(match)) {
+                            done = Integer.valueOf(match);
+                        }
 
                         match = matcher.group(3);
-                        pre = Integer.valueOf(match);
+                        if (StringUtils.isNotEmpty(match)) {
+                            pre = Integer.valueOf(match);
+                        }
 
                         match = matcher.group(4);
-                        queued = Integer.valueOf(match);
+                        if (StringUtils.isNotEmpty(match)) {
+                            queued = Integer.valueOf(match);
+                        }
 
                         match = matcher.group(5);
-                        post = Integer.valueOf(match);
+                        if (StringUtils.isNotEmpty(match)) {
+                            post = Integer.valueOf(match);
+                        }
 
                         match = matcher.group(6);
-                        ready = Integer.valueOf(match);
+                        if (StringUtils.isNotEmpty(match)) {
+                            ready = Integer.valueOf(match);
+                        }
 
                         match = matcher.group(7);
-                        unReady = Integer.valueOf(match);
+                        if (StringUtils.isNotEmpty(match)) {
+                            unReady = Integer.valueOf(match);
+                        }
 
                         match = matcher.group(8);
-                        failed = Integer.valueOf(match);
+                        if (StringUtils.isNotEmpty(match)) {
+                            failed = Integer.valueOf(match);
+                        }
                     }
 
                     String heldLine = br.readLine();
@@ -121,11 +135,14 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
                     matches = matcher.matches();
                     if (matches) {
                         String heldCount = matcher.group(1);
-                        held = Integer.valueOf(heldCount);
+                        if (StringUtils.isNotEmpty(heldCount)) {
+                            held = Integer.valueOf(heldCount);
+                        }
                     }
 
                     String allJobsCompletedLine = br.readLine();
-                    if (allJobsCompletedLine.contains("All jobs Completed")) {
+                    if (StringUtils.isNotEmpty(allJobsCompletedLine)
+                            && allJobsCompletedLine.contains("All jobs Completed")) {
                         allJobsCompleted = true;
                     }
 
