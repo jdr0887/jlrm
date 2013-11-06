44e050f304e94fb63dca6290e85ede7b78a9707b
Jason Reilly <jdr0887@renci.org>
2013-11-06 09:55:09 -0500

    initial commit

---
+++ jlrm-lsf/jlrm-lsf-ssh/src/test/java/org/renci/jlrm/lsf/ssh/Scratch.java
@@ -0,0 +1,12 @@
+package org.renci.jlrm.lsf.ssh;
+
+import org.junit.Test;
+
+public class Scratch {
+
+    @Test
+    public void test() {
+        System.out.println((5760 % 3600));
+        System.out.println((5760 % 3600) / 60);
+    }
+}
