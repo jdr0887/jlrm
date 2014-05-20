51b7aefda7388099df1f4670c28f56ec8dc51add
Jason Reilly <jdr0887@renci.org>
2014-05-20 11:39:33 -0400

    initial commit

---
+++ jlrm-condor/jlrm-condor-api/src/test/java/org/renci/jlrm/condor/Serialization.java
@@ -0,0 +1,49 @@
+package org.renci.jlrm.condor;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
+
+import org.junit.Test;
+
+public class Serialization {
+
+    @Test
+    public void testSerialization() {
+
+        CondorJob job = new CondorJob();
+        job.setDuration(1000);
+        job.setId("asdfads");
+        job.setInitialDirectory(new File("/tmp"));
+
+        job.getClassAdvertisments()
+                .add(new ClassAdvertisement("executable", ClassAdvertisementType.STRING, "sub_1.sh"));
+        job.getClassAdvertisments().add(new ClassAdvertisement("output", ClassAdvertisementType.STRING, "sub_1.out"));
+        job.getClassAdvertisments().add(new ClassAdvertisement("error", ClassAdvertisementType.STRING, "sub_1.err"));
+
+        job.getClassAdvertisments()
+                .add(new ClassAdvertisement("executable", ClassAdvertisementType.STRING, "sub_2.sh"));
+        job.getClassAdvertisments().add(new ClassAdvertisement("output", ClassAdvertisementType.STRING, "sub_2.out"));
+        job.getClassAdvertisments().add(new ClassAdvertisement("error", ClassAdvertisementType.STRING, "sub_2.err"));
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(CondorJob.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            FileWriter fw = new FileWriter(new File("/tmp/condorJob.xml"));
+            m.marshal(job, fw);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (PropertyException e1) {
+            e1.printStackTrace();
+        } catch (JAXBException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+}
