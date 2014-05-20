807532eefc5b4491671de0c9b9e319480169e0bf
Jason Reilly <jdr0887@renci.org>
2014-05-20 11:27:58 -0400

    taking file as argument

--- jlrm-condor/jlrm-condor-cli/src/main/java/org/renci/jlrm/condor/cli/CondorLookupDAGStatusCallable.java
+++ jlrm-condor/jlrm-condor-cli/src/main/java/org/renci/jlrm/condor/cli/CondorLookupDAGStatusCallable.java
@@ -13,7 +13,6 @@ import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.renci.jlrm.JLRMException;
-import org.renci.jlrm.condor.CondorJob;
 import org.renci.jlrm.condor.CondorJobStatusType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,20 +21,31 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
 
     private final Logger logger = LoggerFactory.getLogger(CondorLookupDAGStatusCallable.class);
 
-    private CondorJob job;
+    private File dagmanOutFile;
 
-    public CondorLookupDAGStatusCallable(CondorJob job) {
+    public CondorLookupDAGStatusCallable() {
         super();
-        this.job = job;
+    }
+
+    public CondorLookupDAGStatusCallable(File dagmanOutFile) {
+        super();
+        this.dagmanOutFile = dagmanOutFile;
     }
 
     @Override
     public CondorJobStatusType call() throws JLRMException {
         logger.info("ENTERING call()");
+
+        if (this.dagmanOutFile == null) {
+            throw new JLRMException("dagmanOutFile can't be null");
+        }
+
         CondorJobStatusType ret = CondorJobStatusType.UNEXPANDED;
 
         boolean allJobsCompleted = false;
         boolean dagAborted = false;
+        boolean dagRemoved = false;
+        
         int totalChildrenJobs = 0;
         Date date = null;
         int done = 0;
@@ -47,8 +57,8 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
         int failed = 0;
         int held = 0;
 
-        File dagmanOutFile = new File(job.getSubmitFile().getParentFile(), job.getSubmitFile().getName()
-                .replace(".dag", ".dag.dagman.out"));
+        // File dagmanOutFile = new File(job.getSubmitFile().getParentFile(), job.getSubmitFile().getName()
+        // .replace(".dag", ".dag.dagman.out"));
 
         if (!dagmanOutFile.exists()) {
             ret = CondorJobStatusType.IDLE;
@@ -62,6 +72,7 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
 
             String line;
             while ((line = br.readLine()) != null) {
+
                 if (line.contains("Dag contains")) {
                     Pattern pattern = Pattern.compile("^.+ Dag contains (\\d*) total jobs$");
                     Matcher matcher = pattern.matcher(line);
@@ -69,15 +80,16 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
                     if (matches) {
                         String count = matcher.group(1);
                         totalChildrenJobs = Integer.valueOf(count);
-                        break;
                     }
                 }
-            }
 
-            while ((line = br.readLine()) != null) {
+                if (totalChildrenJobs == 0) {
+                    continue;
+                }
+
                 if (line.contains(String.format("Of %d nodes total", totalChildrenJobs))) {
-                    br.readLine();
-                    br.readLine();
+                    String a = br.readLine();
+                    String b = br.readLine();
                     String tallies = br.readLine();
                     Pattern pattern = Pattern
                             .compile("^(\\d*/\\d*/\\d*\\s.\\d*:\\d*:\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)");
@@ -86,48 +98,32 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
                     if (matches) {
 
                         String match = matcher.group(1);
-                        if (StringUtils.isNotEmpty(match)) {
-                            try {
-                                date = DateUtils.parseDate(match, new String[] { "MM/dd/yy HH:mm:ss" });
-                            } catch (ParseException e) {
-                                e.printStackTrace();
-                            }
+                        try {
+                            date = DateUtils.parseDate(match, new String[] { "MM/dd/yy HH:mm:ss" });
+                        } catch (ParseException e) {
+                            e.printStackTrace();
                         }
 
                         match = matcher.group(2);
-                        if (StringUtils.isNotEmpty(match)) {
-                            done = Integer.valueOf(match);
-                        }
+                        done = Integer.valueOf(match);
 
                         match = matcher.group(3);
-                        if (StringUtils.isNotEmpty(match)) {
-                            pre = Integer.valueOf(match);
-                        }
+                        pre = Integer.valueOf(match);
 
                         match = matcher.group(4);
-                        if (StringUtils.isNotEmpty(match)) {
-                            queued = Integer.valueOf(match);
-                        }
+                        queued = Integer.valueOf(match);
 
                         match = matcher.group(5);
-                        if (StringUtils.isNotEmpty(match)) {
-                            post = Integer.valueOf(match);
-                        }
+                        post = Integer.valueOf(match);
 
                         match = matcher.group(6);
-                        if (StringUtils.isNotEmpty(match)) {
-                            ready = Integer.valueOf(match);
-                        }
+                        ready = Integer.valueOf(match);
 
                         match = matcher.group(7);
-                        if (StringUtils.isNotEmpty(match)) {
-                            unReady = Integer.valueOf(match);
-                        }
+                        unReady = Integer.valueOf(match);
 
                         match = matcher.group(8);
-                        if (StringUtils.isNotEmpty(match)) {
-                            failed = Integer.valueOf(match);
-                        }
+                        failed = Integer.valueOf(match);
                     }
 
                     String heldLine = br.readLine();
@@ -136,23 +132,24 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
                     matches = matcher.matches();
                     if (matches) {
                         String heldCount = matcher.group(1);
-                        if (StringUtils.isNotEmpty(heldCount)) {
-                            held = Integer.valueOf(heldCount);
-                        }
+                        held = Integer.valueOf(heldCount);
                     }
 
-                    String allJobsCompletedLine = br.readLine();
-                    if (StringUtils.isNotEmpty(allJobsCompletedLine)) {
-                        if (allJobsCompletedLine.contains("All jobs Completed")) {
-                            allJobsCompleted = true;
-                        } else if (allJobsCompletedLine.contains("Received SIGUSR1")) {
-                            String abortingDAGLine = br.readLine();
-                            if (abortingDAGLine.contains("Aborting DAG")) {
-                                dagAborted = true;
-                            }
+                }
+
+                if (StringUtils.isNotEmpty(line)) {
+                    if (line.contains("All jobs Completed")) {
+                        allJobsCompleted = true;
+                    }
+                    if (line.contains("Received SIGUSR1")) {
+                        String abortingDAGLine = br.readLine();
+                        if (abortingDAGLine.contains("Aborting DAG")) {
+                            dagAborted = true;
                         }
                     }
-
+                    if (line.contains("Running: condor_rm -const DAGManJobId")) {
+                        dagRemoved = true;
+                    }
                 }
             }
 
@@ -170,7 +167,9 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
 
         boolean completed = (done == totalChildrenJobs && allJobsCompleted) || dagAborted;
         boolean running = queued > 0 && queued < totalChildrenJobs;
-        if (completed) {
+        if (dagRemoved) {
+            ret = CondorJobStatusType.REMOVED;
+        } else if (completed) {
             ret = CondorJobStatusType.COMPLETED;
         } else if (!completed && running) {
             ret = CondorJobStatusType.RUNNING;
@@ -179,12 +178,12 @@ public class CondorLookupDAGStatusCallable implements Callable<CondorJobStatusTy
         return ret;
     }
 
-    public CondorJob getJob() {
-        return job;
+    public File getDagmanOutFile() {
+        return dagmanOutFile;
     }
 
-    public void setJob(CondorJob job) {
-        this.job = job;
+    public void setDagmanOutFile(File dagmanOutFile) {
+        this.dagmanOutFile = dagmanOutFile;
     }
 
 }
