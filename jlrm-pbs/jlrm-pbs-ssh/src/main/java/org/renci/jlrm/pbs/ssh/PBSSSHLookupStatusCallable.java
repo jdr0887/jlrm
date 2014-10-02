843f2111e691ed83bc0fa324e32e9d0040ddec0f
Jason Reilly <jdr0887@gmail.com>
2014-10-02 14:56:16 -0400

    parsing xml from pbs

--- jlrm-pbs/jlrm-pbs-ssh/src/main/java/org/renci/jlrm/pbs/ssh/PBSSSHLookupStatusCallable.java
+++ jlrm-pbs/jlrm-pbs-ssh/src/main/java/org/renci/jlrm/pbs/ssh/PBSSSHLookupStatusCallable.java
@@ -1,13 +1,20 @@
 package org.renci.jlrm.pbs.ssh;
 
 import java.io.IOException;
-import java.io.LineNumberReader;
 import java.io.StringReader;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
-import org.apache.commons.lang.StringUtils;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.apache.commons.lang3.StringUtils;
 import org.renci.jlrm.JLRMException;
 import org.renci.jlrm.Site;
 import org.renci.jlrm.commons.ssh.SSHConnectionUtil;
@@ -15,6 +22,12 @@ import org.renci.jlrm.pbs.PBSJobStatusInfo;
 import org.renci.jlrm.pbs.PBSJobStatusType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 public class PBSSSHLookupStatusCallable implements Callable<Set<PBSJobStatusInfo>> {
 
@@ -38,42 +51,55 @@ public class PBSSSHLookupStatusCallable implements Callable<Set<PBSJobStatusInfo
         Set<PBSJobStatusInfo> jobStatusSet = new HashSet<PBSJobStatusInfo>();
 
         try {
+            String output = SSHConnectionUtil.execute("qstat -fx", site.getUsername(), site.getSubmitHost());
 
-            String command = String.format("qstat -u %s | awk '{print $1,$10,$3,$4}'", site.getUsername());
-            String output = SSHConnectionUtil.execute(command, site.getUsername(), site.getSubmitHost());
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            StringReader sr = new StringReader(output);
+            InputSource inputSource = new InputSource(sr);
+            Document document = documentBuilder.parse(inputSource);
+            XPath xpath = XPathFactory.newInstance().newXPath();
 
-            LineNumberReader lnr = new LineNumberReader(new StringReader(output));
-            boolean canRead = false;
-            String line;
-            while ((line = lnr.readLine()) != null) {
+            NodeList jobNodeList = (NodeList) xpath.evaluate("/Data/Job", document, XPathConstants.NODESET);
 
-                if (StringUtils.isNotEmpty(line)) {
-
-                    if (line.startsWith("---")) {
-                        canRead = true;
-                        continue;
-                    }
-
-                    if (canRead) {
-                        PBSJobStatusType statusType = PBSJobStatusType.ENDING;
-                        String[] lineSplit = line.split(" ");
-                        if (lineSplit != null && lineSplit.length == 4) {
-                            for (PBSJobStatusType type : PBSJobStatusType.values()) {
-                                if (type.getValue().equals(lineSplit[1])) {
-                                    statusType = type;
+            for (int i = 0; i < jobNodeList.getLength(); i++) {
+                Node a = jobNodeList.item(i);
+                NodeList childNodes = a.getChildNodes();
+                for (int j = 0; j < childNodes.getLength(); j++) {
+                    Node b = childNodes.item(j);
+                    PBSJobStatusInfo info = new PBSJobStatusInfo();
+                    String nodeName = b.getNodeName();
+                    if (StringUtils.isNotEmpty(nodeName)) {
+                        switch (nodeName) {
+                            case "Job_Id":
+                                info.setJobId(b.getTextContent());
+                                break;
+                            case "Job_Name":
+                                info.setJobName(b.getTextContent());
+                                break;
+                            case "job_state":
+                                PBSJobStatusType statusType = PBSJobStatusType.ENDING;
+                                for (PBSJobStatusType type : PBSJobStatusType.values()) {
+                                    if (type.getValue().equals(b.getTextContent())) {
+                                        statusType = type;
+                                    }
                                 }
-                            }
-                            String jobId = lineSplit[0].substring(0, lineSplit[0].indexOf("."));
-                            PBSJobStatusInfo info = new PBSJobStatusInfo(jobId, statusType, lineSplit[2], lineSplit[3]);
-                            jobStatusSet.add(info);
+                                info.setType(statusType);
+                                break;
+                            case "queue":
+                                info.setQueue(b.getTextContent());
+                                break;
+                            default:
+                                break;
                         }
+                        jobStatusSet.add(info);
                     }
                 }
             }
-        } catch (IOException e) {
+        } catch (XPathExpressionException | DOMException | ParserConfigurationException | SAXException | IOException e) {
             logger.error("IOException", e);
             throw new JLRMException("IOException: " + e.getMessage());
         }
+
         return jobStatusSet;
     }
 
