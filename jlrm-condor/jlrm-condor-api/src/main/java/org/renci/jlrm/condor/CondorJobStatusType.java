0e7913086864fe455e17f6490ae2c86b90cfa9c8
Jason Reilly <jdr0887@gmail.com>
2016-04-25 13:22:31 -0400

    adding dagjobstatustype

--- jlrm-condor/jlrm-condor-api/src/main/java/org/renci/jlrm/condor/CondorJobStatusType.java
+++ jlrm-condor/jlrm-condor-api/src/main/java/org/renci/jlrm/condor/CondorJobStatusType.java
@@ -18,23 +18,30 @@ public enum CondorJobStatusType {
 
     private int code;
 
+    private CondorDAGJobStatusType dagJobStatusType;
+
     private CondorJobStatusType(int code) {
         this.code = code;
     }
 
-    /**
-     * @return the code
-     */
+    private CondorJobStatusType(int code, CondorDAGJobStatusType dagJobStatusType) {
+        this.code = code;
+    }
+
     public int getCode() {
         return code;
     }
 
-    /**
-     * @param code
-     *            the code to set
-     */
     public void setCode(int code) {
         this.code = code;
+    }
+
+    public CondorDAGJobStatusType getDagJobStatusType() {
+        return dagJobStatusType;
+    }
+
+    public void setDagJobStatusType(CondorDAGJobStatusType dagJobStatusType) {
+        this.dagJobStatusType = dagJobStatusType;
     }
 
 }
