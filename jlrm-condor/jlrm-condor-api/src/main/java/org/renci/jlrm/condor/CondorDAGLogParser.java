7dd6a92ad6aa794d224e069832c8aca2697c53ca
Jason Reilly <jdr0887@gmail.com>
2016-04-25 13:23:10 -0400

    adding new dagjobstatustype

--- jlrm-condor/jlrm-condor-api/src/main/java/org/renci/jlrm/condor/CondorDAGLogParser.java
+++ jlrm-condor/jlrm-condor-api/src/main/java/org/renci/jlrm/condor/CondorDAGLogParser.java
@@ -3,16 +3,26 @@ package org.renci.jlrm.condor;
 import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.renci.jlrm.JLRMException;
 
 public class CondorDAGLogParser {
+
+    private static final Pattern dagStatusPattern = Pattern.compile("^.+DAG status: (\\d*).+$");
+
+    // private static final Pattern dagTotalJobsPattern = Pattern.compile("^.+Of (\\d*) nodes total:$");
+    private static final Pattern dagTotalJobsPattern = Pattern.compile("^.+Dag contains (\\d*) total jobs$");
+
+    private static final Pattern dagExitStatusPattern = Pattern.compile("^.+EXITING WITH STATUS (\\d*)$");
 
     private static CondorDAGLogParser instance = null;
 
@@ -35,75 +45,123 @@ public class CondorDAGLogParser {
 
         CondorJobStatusType ret = CondorJobStatusType.UNEXPANDED;
 
-        boolean hasExited = false;
-        int totalChildrenJobs = 0;
-
         if (!logFile.exists()) {
             ret = CondorJobStatusType.IDLE;
             return ret;
         }
 
+        List<String> lines = new ArrayList<>();
+        // try (RandomAccessFile raf = new RandomAccessFile(logFile, "r")) {
+        // long length = raf.length();
+        // if (length > 1500) {
+        // raf.seek(length - 1500);
+        // String line;
+        // while ((line = raf.readLine()) != null) {
+        // lines.add(line);
+        // }
+        // }
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        // }
+
         try {
-            List<String> lines = FileUtils.readLines(logFile);
-            String lastLine = lines.get(lines.size() - 1);
-
-            if (lastLine.contains("EXITING WITH STATUS")) {
-                // we now know what the dag is finished...just not how
-                hasExited = true;
-            }
-
-            ListIterator<String> lineIter = lines.listIterator();
-            while (lineIter.hasNext()) {
-                String line = lineIter.next();
-                if (line.contains("Dag contains")) {
-                    Pattern pattern = Pattern.compile("^.+ Dag contains (\\d*) total jobs$");
-                    Matcher matcher = pattern.matcher(line);
-                    boolean matches = matcher.matches();
-                    if (matches) {
-                        String count = matcher.group(1);
-                        totalChildrenJobs = Integer.valueOf(count);
-                        break;
-                    }
-                }
-            }
-
-            if (totalChildrenJobs == 0) {
-                // dagman with no children???
-                return CondorJobStatusType.COMPLETED;
-            }
-
-            CondorJobTally tally = getLatestTally(lines, totalChildrenJobs);
-
-            if (hasExited) {
-                // find termination state
-
-                lineIter = lines.listIterator(lines.size());
-                while (lineIter.hasPrevious()) {
-                    String line = lineIter.previous();
-                    if (line.contains("All jobs Completed") && tally.getDone().equals(totalChildrenJobs)) {
-                        ret = CondorJobStatusType.COMPLETED;
-                        break;
-                    }
-                    if (line.contains("Running: condor_rm -const DAGManJobId")) {
-                        ret = CondorJobStatusType.REMOVED;
-                        break;
-                    }
-                    if (line.contains("Aborting DAG") && tally.getFailed() > 0) {
-                        ret = CondorJobStatusType.REMOVED;
-                        break;
-                    }
-                }
-
-            } else {
-
-                if (tally.getQueued() > 0 && tally.getQueued() < totalChildrenJobs) {
-                    ret = CondorJobStatusType.RUNNING;
-                }
-
-            }
-        } catch (IOException e1) {
-            e1.printStackTrace();
+            lines.addAll(FileUtils.readLines(logFile));
+            // Collections.reverse(lines.subList(0, lines.size()));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        Matcher matcher = null;
+
+        int totalChildrenJobs = 0;
+        for (String line : lines) {
+            matcher = dagTotalJobsPattern.matcher(line);
+            if (matcher.matches()) {
+                String count = matcher.group(1);
+                if (StringUtils.isNotEmpty(count)) {
+                    totalChildrenJobs = Integer.valueOf(count);
+                    break;
+                }
+            }
+        }
+
+        String lastLine = lines.get(lines.size() - 1);
+
+        boolean hasExited = false;
+        int exitCode = 0;
+
+        matcher = dagExitStatusPattern.matcher(lastLine);
+        if (matcher.matches()) {
+            hasExited = true;
+            if (matcher.matches()) {
+                String code = matcher.group(1);
+                if (StringUtils.isNotEmpty(code)) {
+                    exitCode = Integer.valueOf(code);
+                }
+            }
+        }
+
+        CondorDAGJobStatusType dagJobStatusType = CondorDAGJobStatusType.OK;
+        ListIterator<String> reverseIter = lines.listIterator(lines.size());
+
+        main: while (reverseIter.hasPrevious()) {
+            String line = reverseIter.previous();
+            matcher = dagStatusPattern.matcher(line);
+            if (matcher.matches()) {
+                String code = matcher.group(1);
+                if (StringUtils.isNotEmpty(code)) {
+                    for (CondorDAGJobStatusType type : CondorDAGJobStatusType.values()) {
+                        if (type.getCode() == Integer.valueOf(code)) {
+                            dagJobStatusType = type;
+                            break main;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (totalChildrenJobs == 0) {
+            // dagman with no children???
+            ret = CondorJobStatusType.COMPLETED;
+            ret.setDagJobStatusType(dagJobStatusType);
+            return ret;
+        }
+
+        CondorJobTally tally = getLatestTally(lines, totalChildrenJobs);
+
+        if (hasExited) {
+            // find termination state
+
+            reverseIter = lines.listIterator(lines.size());
+
+            while (reverseIter.hasPrevious()) {
+                String line = reverseIter.previous();
+
+                if (line.contains("All jobs Completed") && tally.getDone().equals(totalChildrenJobs)) {
+                    ret = CondorJobStatusType.COMPLETED;
+                    break;
+                }
+
+                if (line.contains("Running: condor_rm -const DAGManJobId")) {
+                    ret = CondorJobStatusType.REMOVED;
+                    break;
+                }
+
+                if (line.contains("Aborting DAG") && (tally.getFailed() > 0 || dagJobStatusType.equals(CondorDAGJobStatusType.REMOVED))) {
+                    ret = CondorJobStatusType.REMOVED;
+                    break;
+                }
+                
+            }
+
+        } else {
+
+            if (tally.getQueued() > 0 && tally.getQueued() < totalChildrenJobs) {
+                ret = CondorJobStatusType.RUNNING;
+            }
+
+        }
+        ret.setDagJobStatusType(dagJobStatusType);
 
         return ret;
 
@@ -120,8 +178,8 @@ public class CondorDAGLogParser {
                 lineIter.next();
                 lineIter.next();
                 String tallies = lineIter.next();
-                Pattern pattern = Pattern
-                        .compile("^(\\d*/\\d*/\\d*\\s.\\d*:\\d*:\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)");
+                Pattern pattern = Pattern.compile(
+                        "^(\\d*/\\d*/\\d*\\s.\\d*:\\d*:\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)\\s+(\\d*)");
                 Matcher matcher = pattern.matcher(tallies);
                 boolean matches = matcher.matches();
                 if (matches) {
