046d367c8886075284d8cf65c7747939766beed7
Jason Reilly <jdr0887@gmail.com>
2018-01-30 10:16:46 -0500

    using try w/ resources

--- jlrm-slurm/jlrm-slurm-ssh/src/main/java/org/renci/jlrm/slurm/ssh/SLURMSubmitScriptExporter.java
+++ jlrm-slurm/jlrm-slurm-ssh/src/main/java/org/renci/jlrm/slurm/ssh/SLURMSubmitScriptExporter.java
@@ -1,5 +1,6 @@
 package org.renci.jlrm.slurm.ssh;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -21,62 +22,61 @@ public class SLURMSubmitScriptExporter<T extends SLURMSSHJob> {
         logger.debug("ENTERING export(File, String, T)");
         File submitFile = new File(workDir, String.format("%s.sub", job.getName()));
 
-        FileWriter submitFileWriter = new FileWriter(submitFile);
+        try (FileWriter fw = new FileWriter(submitFile); BufferedWriter bw = new BufferedWriter(fw)) {
 
-        submitFileWriter.write("#!/bin/bash\n\n");
-        submitFileWriter.write(String.format("#SBATCH -J %s%n", job.getName()));
+            bw.write("#!/bin/bash\n\n");
+            bw.write(String.format("#SBATCH -J %s%n", job.getName()));
 
-        if (StringUtils.isNotEmpty(job.getQueueName())) {
-            submitFileWriter.write(String.format("#SBATCH -p %s%n", job.getQueueName()));
-        }
-
-        if (StringUtils.isNotEmpty(job.getProject())) {
-            submitFileWriter.write(String.format("#SBATCH -A %s%n", job.getProject()));
-        }
-
-        if (job.getArray() != null) {
-            Range<Integer> arrayRange = job.getArray();
-            if (arrayRange.getMinimum().equals(arrayRange.getMaximum())) {
-                submitFileWriter.write(String.format("#SBATCH --array=%s%n", job.getArray().getMinimum()));
-            } else {
-                submitFileWriter.write(String.format("#SBATCH --array=%s-%s%n", job.getArray().getMinimum(),
-                        job.getArray().getMaximum()));
+            if (StringUtils.isNotEmpty(job.getQueueName())) {
+                bw.write(String.format("#SBATCH -p %s%n", job.getQueueName()));
             }
+
+            if (StringUtils.isNotEmpty(job.getProject())) {
+                bw.write(String.format("#SBATCH -A %s%n", job.getProject()));
+            }
+
+            if (job.getArray() != null) {
+                Range<Integer> arrayRange = job.getArray();
+                if (arrayRange.getMinimum().equals(arrayRange.getMaximum())) {
+                    bw.write(String.format("#SBATCH --array=%s%n", job.getArray().getMinimum()));
+                } else {
+                    bw.write(String.format("#SBATCH --array=%s-%s%n", job.getArray().getMinimum(),
+                            job.getArray().getMaximum()));
+                }
+            }
+
+            if (job.getWallTime() != null) {
+                bw.write(String.format("#SBATCH -t %d%n", job.getWallTime()));
+            }
+
+            if (StringUtils.isNotEmpty(job.getConstraint())) {
+                bw.write(String.format("#SBATCH -C %s%n", job.getConstraint()));
+            }
+
+            if (job.getMemory() != null) {
+                bw.write(String.format("#SBATCH --mem=%s%n", job.getMemory()));
+            }
+
+            bw.write(String.format("#SBATCH -i %s%n", "/dev/null"));
+
+            bw.write(String.format("#SBATCH -o %s%n", job.getOutput().getAbsolutePath()));
+            bw.write(String.format("#SBATCH -e %s%n", job.getError().getAbsolutePath()));
+
+            if (job.getHostCount() != null) {
+                bw.write(String.format("#SBATCH -N %d%n", job.getHostCount()));
+            }
+
+            if (job.getNumberOfProcessors() != null) {
+                bw.write(String.format("#SBATCH -n %d%n", job.getNumberOfProcessors()));
+            }
+
+            if (job.getTransferExecutable()) {
+                bw.write(remoteWorkDir + File.separator + job.getExecutable().getName());
+            } else {
+                bw.write(job.getExecutable().getAbsolutePath());
+            }
+            bw.flush();
         }
-
-        if (job.getWallTime() != null) {
-            submitFileWriter.write(String.format("#SBATCH -t %d%n", job.getWallTime()));
-        }
-
-        if (StringUtils.isNotEmpty(job.getConstraint())) {
-            submitFileWriter.write(String.format("#SBATCH -C %s%n", job.getConstraint()));
-        }
-
-        if (job.getMemory() != null) {
-            submitFileWriter.write(String.format("#SBATCH --mem=%s%n", job.getMemory()));
-        }
-
-        submitFileWriter.write(String.format("#SBATCH -i %s%n", "/dev/null"));
-
-        submitFileWriter.write(String.format("#SBATCH -o %s%n", job.getOutput().getAbsolutePath()));
-        submitFileWriter.write(String.format("#SBATCH -e %s%n", job.getError().getAbsolutePath()));
-
-        if (job.getHostCount() != null) {
-            submitFileWriter.write(String.format("#SBATCH -N %d%n", job.getHostCount()));
-        }
-
-        if (job.getNumberOfProcessors() != null) {
-            submitFileWriter.write(String.format("#SBATCH -n %d%n", job.getNumberOfProcessors()));
-        }
-
-        if (job.getTransferExecutable()) {
-            submitFileWriter.write(remoteWorkDir + File.separator + job.getExecutable().getName());
-        } else {
-            submitFileWriter.write(job.getExecutable().getAbsolutePath());
-        }
-
-        submitFileWriter.flush();
-        submitFileWriter.close();
         job.setSubmitFile(submitFile);
 
         return job;
@@ -85,40 +85,43 @@ public class SLURMSubmitScriptExporter<T extends SLURMSSHJob> {
     public T export(File workDir, T job) throws IOException {
         logger.debug("ENTERING export(File, LSFJob)");
         File submitFile = new File(workDir, String.format("%s.sub", job.getName()));
-        FileWriter submitFileWriter = new FileWriter(submitFile);
 
-        submitFileWriter.write("#!/bin/bash\n\n");
-        submitFileWriter.write("set -e\n\n");
-        submitFileWriter.write(String.format("#SBATCH -J %s%n", job.getName()));
+        try (FileWriter fw = new FileWriter(submitFile); BufferedWriter bw = new BufferedWriter(fw)) {
 
-        if (StringUtils.isNotEmpty(job.getQueueName())) {
-            submitFileWriter.write(String.format("#SBATCH -p %s%n", job.getQueueName()));
+            bw.write("#!/bin/bash\n\n");
+            bw.write("set -e\n\n");
+            bw.write(String.format("#SBATCH -J %s%n", job.getName()));
+
+            if (StringUtils.isNotEmpty(job.getQueueName())) {
+                bw.write(String.format("#SBATCH -p %s%n", job.getQueueName()));
+            }
+
+            if (StringUtils.isNotEmpty(job.getProject())) {
+                bw.write(String.format("#SBATCH -A %s%n", job.getProject()));
+            }
+
+            if (job.getWallTime() != null) {
+                bw.write(String.format("#SBATCH -t %d%n", job.getWallTime() / 60));
+            }
+
+            if (job.getMemory() != null) {
+                bw.write(String.format("#SBATCH --mem=%s%n", job.getMemory()));
+            }
+
+            bw.write(String.format("#SBATCH -i %s%n", "/dev/null"));
+
+            bw.write(String.format("#SBATCH -o %s%n", job.getOutput().getAbsolutePath()));
+            bw.write(String.format("#SBATCH -e %s%n", job.getError().getAbsolutePath()));
+
+            bw.write(String.format("#SBATCH -N %d%n", job.getHostCount()));
+            bw.write(String.format("#SBATCH -n %d%n", job.getNumberOfProcessors()));
+
+            bw.write(job.getExecutable().getAbsolutePath());
+
+            bw.flush();
+
         }
 
-        if (StringUtils.isNotEmpty(job.getProject())) {
-            submitFileWriter.write(String.format("#SBATCH -A %s%n", job.getProject()));
-        }
-
-        if (job.getWallTime() != null) {
-            submitFileWriter.write(String.format("#SBATCH -t %d%n", job.getWallTime() / 60));
-        }
-
-        if (job.getMemory() != null) {
-            submitFileWriter.write(String.format("#SBATCH --mem=%s%n", job.getMemory()));
-        }
-
-        submitFileWriter.write(String.format("#SBATCH -i %s%n", "/dev/null"));
-
-        submitFileWriter.write(String.format("#SBATCH -o %s%n", job.getOutput().getAbsolutePath()));
-        submitFileWriter.write(String.format("#SBATCH -e %s%n", job.getError().getAbsolutePath()));
-
-        submitFileWriter.write(String.format("#SBATCH -N %d%n", job.getHostCount()));
-        submitFileWriter.write(String.format("#SBATCH -n %d%n", job.getNumberOfProcessors()));
-
-        submitFileWriter.write(job.getExecutable().getAbsolutePath());
-
-        submitFileWriter.flush();
-        submitFileWriter.close();
         job.setSubmitFile(submitFile);
 
         return job;
